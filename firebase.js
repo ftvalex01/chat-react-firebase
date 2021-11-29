5f4d35a42a9d10bf2d8d56b139f9ac3a5228b0e7
alejandro <ftvalex@hotmail.com>
2021-11-29 08:43:05 +0000

    configuracion inicial del chat
    configurando la base de datos para nuestro chat en react

---
+++ firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAsN6EOPztZcd5E7QpcvzNKJqw9-zk24NE",
+  authDomain: "chat-react-97fc9.firebaseapp.com",
+  projectId: "chat-react-97fc9",
+  storageBucket: "chat-react-97fc9.appspot.com",
+  messagingSenderId: "647909376484",
+  appId: "1:647909376484:web:651030b61c8593fa54c7f1"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div>
+            chat
+        </div>
+    )
+}
+
+export default App
